289d5988c71dd184bdd307e34b66342a656f7fff
Praveen Kumar Shrivastav <praveen.sunsetpoint@gmail.com>
2018-09-28 01:07:50 +0530

    FIX: Cleanup the existing value if exist on every initializaton of assets; UI fixed for show button; Datamanager to manage the values of textfield;

--- PKAutofillTextField/Classes/PKAutofillTextField.swift
+++ PKAutofillTextField/Classes/PKAutofillTextField.swift
@@ -9,6 +9,23 @@
 import Foundation
 import UIKit
 
+extension String {
+    
+    func textSize(textFont: UIFont) -> CGSize{
+        
+        let fontAttributes = [NSAttributedStringKey.font: textFont]
+        return (self as NSString).size(withAttributes: fontAttributes as [NSAttributedStringKey : Any])
+    }
+    
+    func prependString(string: String) -> String {
+        return string + self
+    }
+    
+    func appendString(string: String) -> String {
+        return self + string
+    }
+}
+
 enum AutoFillTextFieldError: Error {
     case keyMissing
     case outOfStock
@@ -18,6 +35,8 @@ public protocol PKAutofillTextFieldDelegate: class {
     
     func selectedValue(value: String)
 }
+
+//MARK: PKAutofillTextField class
 
 @IBDesignable
 open class PKAutofillTextField: UITextField {
@@ -30,40 +49,72 @@ open class PKAutofillTextField: UITextField {
     var showButton = UIButton(type: .custom)
     var values = Set<String>()
     
-    required public init(frame: CGRect, presenting: UIViewController, keyIdentifier: String, delegate: PKAutofillTextFieldDelegate) {
+    required public init(frame: CGRect, presenting: UIViewController, keyIdentifier: String, delegate: PKAutofillTextFieldDelegate, buttonTitle: String) {
         
         super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.white
+        
+        self.autocapitalizationType = .none
+        
+        self.presentingVC = presenting
+        self.keyIdentifier = keyIdentifier
+        
+        // cleanup old data if exists, for the same key
+        cleanup(key: keyIdentifier)
         
         //UITextField delegate
         self.delegate = self
         
         //PKAutofillTextField delegate
         self.autofillTextFieldDelegate = delegate
-        
-        self.presentingVC = presenting
-        self.keyIdentifier = keyIdentifier
-        
+    
+        // init of tableview to show the values
         self.listVC = PKAutoListViewController.init(delegate: self)
         
-        showButton.titleLabel?.text = "Show"
-        showButton.titleLabel?.textColor = UIColor.black
-        showButton.setTitle("show", for: .normal)
-
-        showButton.frame = CGRect(x: 0, y: 0, width: CGFloat(25), height: CGFloat(25))
-        showButton.addTarget(self, action: #selector(self.refresh), for: .touchUpInside)
+        // add show button
+        addShowButton(title: buttonTitle.prependString(string: "| "))
+    }
+    
+    private func addShowButton(title: String) {
+        
+        //set button title
+        showButton.setTitle(title, for: .normal)
+        
+        // set button frame size based on text size
+        let size = title.textSize(textFont: (showButton.titleLabel?.font)!)
+        showButton.frame = CGRect(x: 0, y: 0, width: size.width + 10, height: self.frame.size.height)
+        
+        // button configuration related to styles
+        showButton.setTitleColor(UIColor.lightGray, for: .normal)
+        showButton.titleLabel?.textAlignment = .center
+        
+        // add action for button
+        showButton.addTarget(self, action: #selector(self.showTableView), for: .touchUpInside)
+        
         self.rightView = showButton
-        self.rightViewMode = .whileEditing
+        self.rightViewMode = .unlessEditing
+    }
+
+    public func defaultValues(values: Array<String>) {
+        
+        for value in values {
+            saveValue(value: value)
+        }
     }
     
-    public func setButtonImage(imageName: String) {
-        showButton.setImage(UIImage(named: imageName), for: .normal)
-    }
-    
-    @IBAction func refresh(_ sender: Any) {
+    @IBAction func showTableView(_ sender: Any) {
         let items = datasource()
         print("items \(items)")
         if (items.count > 0) {
             displayItems()
+        }
+    }
+    
+    private func cleanup(key: String) {
+        let items = datasource()
+        if items.count > 0 {
+            PKDataManager.clearAll(forKey: key)
         }
     }
     
@@ -75,6 +126,7 @@ open class PKAutofillTextField: UITextField {
 extension PKAutofillTextField: PKAutoListViewControllerDelegate {
     
     public func selectedValue(value: String) {
+        self.text = value
         self.listVC?.dismiss(animated: true, completion: nil)
         self.autofillTextFieldDelegate?.selectedValue(value: value)
     }
@@ -146,4 +198,88 @@ extension PKAutofillTextField: UITextFieldDelegate {
         presentationController.permittedArrowDirections = [.down]
         self.presentingVC!.present(controller, animated: true)
     }
+}
+
+//MARK: PKDataManager class
+// This class is intended to execute the data operation like store, retrieval, removal of items
+
+class PKDataManager {
+    
+    static let defaults = UserDefaults.standard
+    
+    static func isKeyPresentInUserDefaults(key: String) -> Bool {
+        return defaults.object(forKey: key) != nil
+    }
+    
+    static func set(_ values: Set<String>, forKey key: String) {
+        
+        let encodedData = NSKeyedArchiver.archivedData(withRootObject: values)
+        defaults.set(encodedData, forKey: key)
+        synchronize()
+    }
+    
+    static func get(forKey key: String) -> Set<String>{
+        
+        if isKeyPresentInUserDefaults(key: key) {
+            
+            let decoded = defaults.value(forKey: key)
+            let decodedTeams = NSKeyedUnarchiver.unarchiveObject(with: decoded as! Data )
+            return (decodedTeams as? Set<String>)!
+        }
+        else {
+            print("Key \(key) is not found")
+        }
+        return []
+    }
+    
+    static func clearAll(forKey key: String) {
+        if isKeyPresentInUserDefaults(key: key) {
+            defaults.removeObject(forKey: key)
+            synchronize()
+        }
+    }
+    
+    static func synchronize() {
+        defaults.synchronize()
+    }
+    
+    
+    static func remove(_ value: String, forKey key: String) {
+        var items = self.get(forKey: key)
+        items.remove(value)
+        
+        clearAll(forKey: key)
+        
+        set(items, forKey: key)
+        
+        synchronize()
+    }
+}
+
+//MARK: PKPopover class
+// This class in intended to display items as popover for both iPad & iPhone
+
+class PKPopover : NSObject, UIPopoverPresentationControllerDelegate {
+    
+    private static let sharedInstance = PKPopover()
+    
+    private override init() {
+        super.init()
+    }
+    
+    /* For iOS8.0, the only supported [adaptive presentation styles] are UIModalPresentationFullScreen and UIModalPresentationOverFullScreen. */
+    //so we are setting as none
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    static func configurePresentation(forController controller : UIViewController) -> UIPopoverPresentationController {
+        
+        controller.modalPresentationStyle = .popover
+        let presentationController = controller.presentationController as! UIPopoverPresentationController
+        presentationController.delegate = PKPopover.sharedInstance
+        
+        return presentationController
+    }
+    
 }
